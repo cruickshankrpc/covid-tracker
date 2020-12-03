1d65aa2936cdae82849bc3aa514bcb3db041dc8e
Raquel <cruickshankrpc@gmail.com>
2020-12-03 16:45:34 +0000

    heading, material UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM naming convention
+    <div className="app">
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+        {/* MATERIAL UI */}
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Option 4</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      {/* HEADER */}
+      {/* TITLE + SELECT DROPDOWN */}
+      {/* INFOBOX */}
+      {/* INFOBOX */}
+      {/* INFOBOX */}
+      {/* TABLE */}
+      {/* GRAPH */}
+      {/* MAP */}
     </div>
   );
 }
